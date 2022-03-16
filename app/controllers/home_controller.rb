d30510af1da0478b84ee5fa584acf7ac16d14f1e
zakino123 <15rb076@ms.dendai.ac.jp>
2022-03-16 13:21:25 +0900

    perform_nowで同期処理

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,21 +7,6 @@ class HomeController < ApplicationController
   end
 
   def call_job_synchronized
-    logger.debug "Sample Job Start."
-
-    job_status = JobStatus.last
-
-    if job_status.blank?
-      logger.debug "no status error."
-      return
-    end
-
-    for i in 0..4
-      job_status.status += 1
-      job_status.save!
-      sleep(3)
-    end
-
-    logger.debug "Sample Job End."
+    SampleJob.perform_now
   end
 end
