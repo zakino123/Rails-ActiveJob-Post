2720e9c683dde95635774b6db6959d29facec9b2
zakino123 <15rb076@ms.dendai.ac.jp>
2022-03-16 12:54:31 +0900

    call_job_synchronizedメソッド実装(同期処理)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,23 @@ class HomeController < ApplicationController
   def call_job
     SampleJob.perform_later
   end
+
+  def call_job_synchronized
+    logger.debug "Sample Job Start."
+
+    job_status = JobStatus.last
+
+    if job_status.blank?
+      logger.debug "no status error."
+      return
+    end
+
+    for i in 0..4
+      job_status.status += 1
+      job_status.save!
+      sleep(3)
+    end
+
+    logger.debug "Sample Job End."
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
   get 'home/call_job'
+  # get 'home/call_job_synchronized'
   root to: 'home#index'
   resources :job_statuses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
