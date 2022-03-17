34e22e0a776c18bb9a4a39c702cea9825840a0fb
zakino123 <15rb076@ms.dendai.ac.jp>
2022-03-17 17:45:24 +0900

    samplejobクラスに毎行デバッグ追加

--- app/jobs/sample_job.rb
+++ app/jobs/sample_job.rb
@@ -9,15 +9,21 @@ class SampleJob < ApplicationJob
 
     job_status = JobStatus.last
 
+    logger.debug "job_status get"
+
     if job_status.blank?
       logger.debug "no status error."
       return
     end
 
+    logger.debug "start Iterative processing"
     for i in 0..4
       job_status.status += 1
+      logger.debug "job_status +1"
       job_status.save!
+      logger.debug "job_status update"
       sleep(3)
+      logger.debug "finish sleep"
     end
 
     logger.debug "Sample Job End."
